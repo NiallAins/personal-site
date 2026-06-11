ba585c6743edc60fc418e825a06116d8b8664207
NiallAins <niallainsworth@gmail.com>
2026-06-11 20:15:41 +0100

    Updated push script

--- push.sh
+++ push.sh
@@ -6,8 +6,8 @@ git push origin main;
 
 if [ $1 == "deploy" ]; then
     git checkout prod;
-    git pull origin prod;
     git reset --hard origin/main;
+    git pull origin prod;
     npm run build-script-prod;
     git add *;
     git commit -m "Pull from main and deploy";
