bf4b4007616af53f133d74eeded64c6715abb11b
NiallAins <niallainsworth@gmail.com>
2026-06-15 11:40:05 +0100

    Simplify push script

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ git push origin main;
 
 if [ $1 == "deploy" ]; then
     git checkout prod;
-    git merge -X theirs main "Auto-deploy from main";
+    git merge -X theirs main -m "Auto-deploy from main";
     npm run build-script-prod;
     git commit -ma "Auto-deploy from main";
     git push origin prod;
