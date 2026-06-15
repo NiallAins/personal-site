7e5e3ca7009021b0ded5652c6d013acac9d2d52a
NiallAins <niallainsworth@gmail.com>
2026-06-15 12:38:10 +0100

    Refacotr build scripts

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git checkout prod;
-git merge -X theirs main -m "Auto-deploy from main";
-npm run build-script-prod;
-git add *;
-git commit -m "Auto-deploy from main";
-git push origin prod;
-git checkout main;
+git checkout prod &&
+git merge -X theirs main -m "Auto-deploy from main" &&
+npm run build-script-prod &&
+git add * &&
+git commit -m "Auto-deploy from main" &&
+git push origin prod &&
+git checkout main
