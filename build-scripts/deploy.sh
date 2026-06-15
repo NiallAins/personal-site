b7ce4c7afa9e5e0fd348046406e6c7aeb96788c9
NiallAins <niallainsworth@gmail.com>
2026-06-15 13:33:20 +0100

    Fix deploy script

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-git checkout prod &
-git reset origin/main --hard &&
-git pull origin main &&
-npm run build-script-prod &&
-git add * &&
-git commit -m "Auto-deploy" &&
-git push origin prod &&
-git checkout main
+git checkout prod;
+git reset origin/main --hard;
+npm run build-script-prod;
+git add *;
+git commit -m "Auto-deploy";
+git push origin prod -f;
+git checkout main;
