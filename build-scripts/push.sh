5e462922abdf15abfdd25603063894e7809f42e1
NiallAins <niallainsworth@gmail.com>
2026-09-24 13:04:05 +0100

    update: build script output colours

--- build-scripts/push.sh
+++ build-scripts/push.sh
@@ -12,7 +12,8 @@ if [ "$1" ]; then
     git commit -m "$1";
 
     # Confirm push after commit outputs
-    read -p "\n${CB}Complete push? (y)${CW} " complete;
+    echo -e "\n${CB}Complete push? (y)${CW} " &&
+    read complete;
     if [ "$complete" = "y" ]; then
         git push origin main &&
         echo -e "\n${CG}Push successful${CW}\n" &&
