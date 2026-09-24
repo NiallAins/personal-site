844725d9d554da145a899bd9356f048afd8268ff
NiallAins <niallainsworth@gmail.com>
2026-09-24 13:17:31 +0100

    update: build script output colours

--- build-scripts/push.sh
+++ build-scripts/push.sh
@@ -6,15 +6,28 @@ CG='\033[0;32m';
 CB='\033[0;36m';
 CW='\033[0m';
 
+output() {
+    local C=${2:CW};
+    echo -e -n "\n${C}$1${CW} ";
+
+    if [ "$3" != "n" ]; then
+        echo '';
+    fi
+}
+
+output test CR
+
+
 if [ "$1" ]; then
     # Push to main
     git add . &&
     git commit -m "$1";
 
     # Confirm push after commit outputs
-    echo -e -n "\n${CB}Complete push? (y)${CW} " &&
+    output "Complete push? (y)" CB n &&
     read resp;
-    echo '';
+    output "";
+    exit 1;
     if [ "$resp" = "y" ]; then
         git push origin main &&
         echo -e "\n${CG}Push successful${CW}\n" &&
